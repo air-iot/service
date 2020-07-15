c52632146966e067faf9bca691f73240bcf9e2cb
Restia <1625016239@qq.com>
2020-07-15 16:32:17 +0800

    增加处理用户名

--- model/warning.go
+++ model/warning.go
@@ -6,28 +6,29 @@ import (
 )
 
 type WarningMessage struct {
-	Id         interface{}              `json:"id"`
-	MainType   string                   `json:"mainType"`
-	Type       string                   `json:"type"`
-	Status     string                   `json:"status"`
-	Processed  string                   `json:"processed"`
-	Parent     []primitive.ObjectID     `json:"status"`
-	Desc       string                   `json:"desc"`
-	Level      string                   `json:"level"`
-	Department []primitive.ObjectID     `json:"department"`
-	Fields     []map[string]interface{} `json:"fields"`
-	Model      interface{}              `json:"model"`
-	ModelID    primitive.ObjectID       `json:"modelID"`
-	NodeID     primitive.ObjectID       `json:"nodeID"`
-	Node       interface{}              `json:"node"`
-	RuleID     string                   `json:"ruleid"`
-	Time       int64                    `json:"time"`
-	Interval   int64                    `json:"interval"`
-	Uid        string                   `json:"uid"`
-	Handle     bool                     `json:"handle"`
-	Alert      bool                     `json:"alert"`
-	Other      map[string]interface{}   `json:"other"`
-	Driver     []primitive.ObjectID     `json:"driver"`
+	Id             interface{}              `json:"id"`
+	MainType       string                   `json:"mainType"`
+	Type           string                   `json:"type"`
+	Status         string                   `json:"status"`
+	Processed      string                   `json:"processed"`
+	Parent         []primitive.ObjectID     `json:"status"`
+	Desc           string                   `json:"desc"`
+	Level          string                   `json:"level"`
+	Department     []primitive.ObjectID     `json:"department"`
+	Fields         []map[string]interface{} `json:"fields"`
+	Model          interface{}              `json:"model"`
+	ModelID        primitive.ObjectID       `json:"modelID"`
+	NodeID         primitive.ObjectID       `json:"nodeID"`
+	Node           interface{}              `json:"node"`
+	RuleID         string                   `json:"ruleid"`
+	Time           int64                    `json:"time"`
+	Interval       int64                    `json:"interval"`
+	Uid            string                   `json:"uid"`
+	Handle         bool                     `json:"handle"`
+	Alert          bool                     `json:"alert"`
+	Other          map[string]interface{}   `json:"other"`
+	Driver         []primitive.ObjectID     `json:"driver"`
+	HandleUserName string                   `json:"handleUserName"`
 }
 
 type WarningSaveMessage struct {
