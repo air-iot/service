9d973ca15dee342dc1270119a1448b25571470f0
Restia <1625016239@qq.com>
2020-10-14 14:14:58 +0800

    Revert "去除发送ws"
    This reverts commit 4f1bc7f07af9f79378532066c8f191de40ebd8a2.

--- tools/websocket.go
+++ tools/websocket.go
@@ -11,17 +11,17 @@ func WebsocketResponse(ws *websocket.Conn, requestId string, statusCode int, dat
 		"code":      statusCode,
 		"data":      data,
 	}
-	_,err := json.Marshal(sendMap)
+	sendByte,err := json.Marshal(sendMap)
 	if err != nil {
 		//return fmt.Errorf("ID为(%s)序列化要发送的数据失败:%s",requestId, err.Error())
 		return fmt.Errorf("序列化要发送的数据失败:%s", err.Error())
 
 	}
-	//err = websocket.Message.Send(ws, string(sendByte))
-	//if err != nil {
-	//	//return fmt.Errorf("ID为(%s)发送Websocket正常响应消息失败:%s", requestId, err.Error())
-	//	return fmt.Errorf("发送Websocket正常响应消息失败:%s", err.Error())
-	//}
+	err = websocket.Message.Send(ws, string(sendByte))
+	if err != nil {
+		//return fmt.Errorf("ID为(%s)发送Websocket正常响应消息失败:%s", requestId, err.Error())
+		return fmt.Errorf("发送Websocket正常响应消息失败:%s", err.Error())
+	}
 	return nil
 }
 
