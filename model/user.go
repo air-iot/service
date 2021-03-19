d74b215e1a7c659be4889bf3d2157f89cc9802db
Restia <1625016239@qq.com>
2021-03-19 16:24:50 +0800

    fix: 修改用户缓存结构，增加使用时长和判断可访问admin页面字段

--- model/user.go
+++ model/user.go
@@ -4,20 +4,24 @@ import "go.mongodb.org/mongo-driver/bson/primitive"
 
 // User
 type User struct {
-	ID         string   `json:"id"`
-	Name       string   `json:"name"`
-	Department []string `json:"department"`
-	Roles      []string `json:"roles"`
-	OpenID     string   `json:"openid"`
-	Phone      string   `json:"phone"`
+	ID             string   `json:"id"`
+	Name           string   `json:"name"`
+	Department     []string `json:"department"`
+	Roles          []string `json:"roles"`
+	OpenID         string   `json:"openid"`
+	Phone          string   `json:"phone"`
+	TimeoutTimeUse string   `json:"timeoutTimeUse"`
+	AdminAccess    bool     `json:"adminAccess"`
 }
 
 // UserMongo
 type UserMongo struct {
-	ID         primitive.ObjectID   `json:"id" bson:"_id"`
-	Name       string               `json:"name" bson:"name"`
-	Department []primitive.ObjectID `json:"department" bson:"department"`
-	Roles      []primitive.ObjectID `json:"roles" bson:"roles"`
-	OpenID     string               `json:"openid" bson:"openid"`
-	Phone      string               `json:"phone" bson:"phone"`
+	ID             primitive.ObjectID   `json:"id" bson:"_id"`
+	Name           string               `json:"name" bson:"name"`
+	Department     []primitive.ObjectID `json:"department" bson:"department"`
+	Roles          []primitive.ObjectID `json:"roles" bson:"roles"`
+	OpenID         string               `json:"openid" bson:"openid"`
+	Phone          string               `json:"phone" bson:"phone"`
+	TimeoutTimeUse interface{}          `json:"timeoutTimeUse" bson:"timeoutTimeUse"`
+	AdminAccess    bool                 `json:"adminAccess" bson:"adminAccess"`
 }
