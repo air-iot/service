4312807a0b9a9df547f5f81806d6afb45d53fd1a
Restia <1625016239@qq.com>
2021-03-26 18:20:00 +0800

    fix: 增加数字和时间的工具方法

---
+++ util/number/number.go
@@ -0,0 +1,125 @@
+package tools
+
+import (
+	"fmt"
+	"math/rand"
+	"regexp"
+	"strconv"
+	"strings"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+func IsNumber(data string) bool {
+	pattern, _ := regexp.Compile("^-?\\d+(\\.\\d+)?$")
+	return pattern.MatchString(data)
+}
+
+func GetIntNumberFromMongoDB(data primitive.M, key string) (int, error) {
+	number := 0
+	numberInDB, ok := data[key].(int32)
+	if !ok {
+		numberInDBFloat, ok := data[key].(float64)
+		if !ok {
+			numberInDBInt64, ok := data[key].(int64)
+			if !ok {
+				numberInDBInt, ok := data[key].(int)
+				if !ok {
+					return 0, fmt.Errorf("该字段不是数字")
+				} else {
+					number = int(numberInDBInt)
+				}
+			} else {
+				number = int(numberInDBInt64)
+			}
+		} else {
+			number = int(numberInDBFloat)
+		}
+	} else {
+		number = int(numberInDB)
+	}
+	return number, nil
+}
+
+func GetFloat64NumberFromMongoDB(data primitive.M, key string) (float64, error) {
+	number := float64(0)
+	numberInDB, ok := data[key].(int32)
+	if !ok {
+		numberInDBFloat, ok := data[key].(float64)
+		if !ok {
+			numberInDBInt64, ok := data[key].(int64)
+			if !ok {
+				numberInDBInt, ok := data[key].(int)
+				if !ok {
+					return 0, fmt.Errorf("该字段不是数字")
+				} else {
+					number = float64(numberInDBInt)
+				}
+			} else {
+				number = float64(numberInDBInt64)
+			}
+		} else {
+			number = float64(numberInDBFloat)
+		}
+	} else {
+		number = float64(numberInDB)
+	}
+	return number, nil
+}
+
+func GetFloatNumber(data interface{}) (float64, error) {
+	number := float64(0)
+	numberInDBFloat32, ok := data.(float32)
+	if !ok {
+		numberInDB, ok := data.(int32)
+		if !ok {
+			numberInDBFloat, ok := data.(float64)
+			if !ok {
+				numberInDBInt64, ok := data.(int64)
+				if !ok {
+					numberInDBInt, ok := data.(int)
+					if !ok {
+						return 0, fmt.Errorf("该字段不是数字")
+					} else {
+						number = float64(numberInDBInt)
+					}
+				} else {
+					number = float64(numberInDBInt64)
+				}
+			} else {
+				number = float64(numberInDBFloat)
+			}
+		} else {
+			number = float64(numberInDB)
+		}
+	} else {
+		number = float64(numberInDBFloat32)
+	}
+	return number, nil
+}
+
+func GetRandomNumberCode(digit int) string {
+	code := ""
+	for i := 1; i <= digit; i++ {
+		code += strconv.Itoa(rand.Intn(10))
+	}
+	return code
+}
+
+func FormatStringToNumber(v string) (interface{}, error) {
+	v = strings.TrimSpace(v)
+	if strings.Contains(v, ".") {
+		res, err := strconv.ParseFloat(v, 64)
+		if err != nil {
+			return nil, err
+		}
+		return res, nil
+	} else {
+		res, err := strconv.Atoi(v)
+		if err != nil {
+			return nil, err
+		}
+		return res, nil
+	}
+
+}
---
+++ util/xtime/time.go
@@ -0,0 +1,145 @@
+package tools
+
+import (
+	"strconv"
+	"time"
+)
+
+func mergeTimeString(dateString *string, timeUnit string, value int64) {
+	if value != 0 {
+		*dateString = strconv.Itoa(int(value)) + timeUnit
+	}
+}
+
+//获取时间间隔转化为天小时分秒
+func GetFormatTimeSpanString(span int64) string {
+	var hour int64
+	var minute int64
+
+	day := span / (3600 * 24)
+
+	if day > 0 {
+		hour = (span / 3600) % 24
+	} else {
+		hour = span / 3600
+	}
+
+	if hour > 0 {
+		minute = (span / 60) % 60
+	} else {
+		minute = span / 60
+	}
+
+	sec := span % 60
+
+	dayString := ""
+	hourString := ""
+	minuteString := ""
+	secString := ""
+
+	mergeTimeString(&dayString, "天", day)
+	mergeTimeString(&hourString, "小时", hour)
+	mergeTimeString(&minuteString, "分", minute)
+	mergeTimeString(&secString, "秒", sec)
+
+	return dayString + hourString + minuteString + secString
+}
+
+//格式字符串转Time
+//time.ParseInLocation("20060102150405","20180223100000",time.Local)
+func ConvertStringToTime(layout, value string, location *time.Location) (*time.Time, error) {
+	t, err := time.ParseInLocation(layout, value, location)
+	if err != nil {
+		return nil, err
+	}
+	return &t, nil
+}
+
+// GetLocalTime 获取本地时区的时间（东八）
+func GetLocalTime(data time.Time, format string) string {
+	cstZone := time.FixedZone("CST", 8*3600) // 东八
+	return data.In(cstZone).Format(format)
+}
+
+// GetLocalTime 获取本地时区的时间（东八）
+func GetLocalTimeNow(data time.Time) time.Time {
+	cstZone := time.FixedZone("CST", 8*3600) // 东八
+	return data.In(cstZone)
+}
+
+// GetLocalTime 获取本地时区的时间（东八）
+func GetUTCTimeNow(data time.Time) time.Time {
+	cstZone := time.FixedZone("UTC", 0) // 0时区
+	return data.In(cstZone)
+}
+
+//循环中获取前几个小时时间
+func GetLastServeralHoursFromZero(i int) time.Time {
+	currentHour, _ := strconv.Atoi(time.Now().Format("15"))
+
+	oldHour := currentHour - i
+	t := time.Date(time.Now().Year(), time.Now().Month(), time.Now().Day(), oldHour, 0, 0, 0, time.Local)
+	return t
+}
+
+//循环中获取前几分钟时间
+func GetLastServeralMinuteFromZero(i int) time.Time {
+	currentMinute  := time.Now().Minute()
+	oldMinute := currentMinute - i
+	t := time.Date(time.Now().Year(), time.Now().Month(), time.Now().Day(), time.Now().Hour(), oldMinute, 0, 0, time.Local)
+	return t
+}
+
+//前推到几天前0点
+func GetUnixToOldTimeDay(i int) time.Time {
+	day := time.Now().Day()
+
+	oldMonth := day - i
+	t := time.Date(time.Now().Year(), time.Now().Month(), oldMonth, 0, 0, 0, 0, time.Local)
+	return t
+}
+
+//前推到几天后0点
+func GetUnixToNewTimeDay(i int) time.Time {
+	day := time.Now().Day()
+
+	oldMonth := day + i
+	t := time.Date(time.Now().Year(), time.Now().Month(), oldMonth, 0, 0, 0, 0, time.Local)
+	return t
+}
+
+//几个月前的月初
+func GetUnixToOldTime(i int) time.Time {
+	currentMonth := int(time.Now().Month())
+
+	oldMonth := currentMonth - i
+	t := time.Date(time.Now().Year(), time.Month(oldMonth), 1, 0, 0, 0, 0, time.Local)
+	return t
+}
+
+//后几个月的月初，指定时间
+func GetUnixToNewTime(i int, timestamp int64) time.Time {
+	currentMonth := int(time.Unix(timestamp, 0).Month())
+
+	newMonth := currentMonth + i
+	t := time.Date(time.Unix(timestamp, 0).Year(), time.Month(newMonth), 1, 0, 0, 0, 0, time.Local)
+	return t
+}
+
+//几年前的当月初
+func GetUnixToOldYearTime(i, month int) time.Time {
+	currentYear := int(time.Now().Year())
+	currentMonth := int(time.Now().Month())
+	oldYear := currentYear - i
+	newMonth := currentMonth - month
+	t := time.Date(oldYear, time.Month(newMonth), 1, 0, 0, 0, 0, time.Local)
+	return t
+}
+
+//几年前的季度
+func GetUnixToOldYearQuarterTime(i, month int) time.Time {
+	currentYear := int(time.Now().Year())
+	oldYear := currentYear - i
+	t := time.Date(oldYear, time.Month(month), 1, 0, 0, 0, 0, time.Local)
+	return t
+}
