f75948eff5bc2f6e4968f02d52a60a0ec60b658f
Restia <1625016239@qq.com>
2021-03-29 15:19:09 +0800

    fix: 修改mongo方法多余部分和错误

--- init/mongodb/db.go
+++ init/mongodb/db.go
@@ -17,9 +17,6 @@ import (
 type (
 	// PipelineFunc 声明了一个Pipeline修改函数类型
 	PipelineFunc func() mongo.Pipeline
-
-	// CalcPipelineFunc 声明了一个修改返回值函数
-	CalcPipelineFunc func(*bson.M)
 )
 
 // ConvertKeyID 转换查询的ObjectID
@@ -45,15 +42,6 @@ func ConvertKeyID(data *bson.M) error {
 				return err
 			}
 			(*data)[k] = value
-		case primitive.ObjectID:
-			value, err := ConvertObjectIDToID(data, k, val)
-			if err != nil {
-				return err
-			}
-			if value != nil {
-				delete(*data, k)
-				(*data)["id"] = value
-			}
 		case string:
 			if k == "_id" {
 				delete(*data, k)
@@ -63,14 +51,6 @@ func ConvertKeyID(data *bson.M) error {
 	}
 
 	return nil
-}
-
-// ConvertObjectIDToID 转换字符串类型
-func ConvertObjectIDToID(data *bson.M, key string, val primitive.ObjectID) (interface{}, error) {
-	if key == "_id" {
-		return val, nil
-	}
-	return nil, nil
 }
 
 // ConvertPrimitiveAToID 转换interface类型数组
@@ -92,7 +72,7 @@ func ConvertPrimitiveAToID(data *bson.M, key string, val primitive.A) (interface
 			}
 			result = append(result, value)
 			//(*data)[k] = value
-		case primitive.ObjectID, string:
+		case string:
 			result = append(result, val)
 		default:
 			result = append(result, val)
@@ -118,17 +98,6 @@ func ConvertPrimitiveMapToID(data *bson.M, key string, value primitive.M) (inter
 				return nil, err
 			}
 			value[k] = r
-		case primitive.ObjectID:
-			r, err := ConvertObjectIDToID(data, k, val)
-			if err != nil {
-				return nil, err
-			}
-			if r != nil {
-				delete(value, k)
-				value["id"] = r
-			} else {
-				value[k] = val
-			}
 		case string:
 			if k == "_id" {
 				delete(value, k)
@@ -193,7 +162,7 @@ func FindQueryConvertPipeline(query bson.M) (mongo.Pipeline, mongo.Pipeline, err
 
 // FindFilter 根据条件查询数据(原版)
 // result:查询结果 query:查询条件
-func FindFilter(ctx context.Context, col *mongo.Collection, result *[]bson.M, query bson.M, calc CalcPipelineFunc) (int, error) {
+func FindFilter(ctx context.Context, col *mongo.Collection, result *[]bson.M, query bson.M) (int, error) {
 
 	var count int
 	projectMap := bson.M{}
@@ -477,7 +446,7 @@ func FindFilter(ctx context.Context, col *mongo.Collection, result *[]bson.M, qu
 	//	pipeLine = append(pipeLine, bson.D{bson.E{Key: "$project", Value: project}})
 	//}
 	//newPipeLine := DeepCopy(pipeLine).(mongo.Pipeline)
-	if err := FindPipeline(ctx, col, result, pipeLine, calc); err != nil {
+	if err := FindPipeline(ctx, col, result, pipeLine); err != nil {
 		return 0, err
 	}
 	return count, nil
@@ -485,7 +454,7 @@ func FindFilter(ctx context.Context, col *mongo.Collection, result *[]bson.M, qu
 
 // FindFilter 根据条件查询数据(原版)
 // result:查询结果 query:查询条件
-//func FindFilterDeptQuery(ctx context.Context, col *mongo.Collection, result *[]bson.M, query bson.M, calc CalcPipelineFunc) (int, error) {
+//func FindFilterDeptQuery(ctx context.Context, col *mongo.Collection, result *[]bson.M, query bson.M) (int, error) {
 //
 //	var count int
 //	projectMap := bson.M{}
@@ -753,7 +722,7 @@ func FindFilter(ctx context.Context, col *mongo.Collection, result *[]bson.M, qu
 
 // FindFilterLimit 根据条件查询数据(先根据条件分页)
 // result:查询结果 query:查询条件
-func FindFilterLimit(ctx context.Context, col *mongo.Collection, result *[]bson.M, query bson.M, calc CalcPipelineFunc) (int, error) {
+func FindFilterLimit(ctx context.Context, col *mongo.Collection, result *[]bson.M, query bson.M) (int, error) {
 
 	var count int
 	projectMap := bson.M{}
@@ -850,17 +819,9 @@ func FindFilterLimit(ctx context.Context, col *mongo.Collection, result *[]bson.
 									if k == "id" {
 										k = "_id"
 									}
-									//if strings.HasSuffix(k, "id") && k != "_id" && !strings.HasSuffix(k, "_id") && !strings.HasSuffix(k, "uid") {
-									//	k = k[:len(k)-2]
-									//	k = k + "_id"
-									//} else
-									if hasGroup && k == "modelId" {
-										k = "model"
-									} else {
-										if strings.HasSuffix(k, "Id") && k != "requestId" {
-											k = k[:len(k)-2]
-											k = k + "._id"
-										}
+									if strings.HasSuffix(k, "Id") && k != "requestId" {
+										k = k[:len(k)-2]
+										k = k + "._id"
 									}
 									pipeLine = append(pipeLine, bson.D{bson.E{Key: "$match", Value: bson.M{k: v}}})
 								}
@@ -902,13 +863,9 @@ func FindFilterLimit(ctx context.Context, col *mongo.Collection, result *[]bson.
 									//	k = k[:len(k)-2]
 									//	k = k + "_id"
 									//} else
-									if hasGroup && k == "modelId" {
-										k = "model"
-									} else {
-										if strings.HasSuffix(k, "Id") && k != "requestId" {
-											k = k[:len(k)-2]
-											k = k + "._id"
-										}
+									if strings.HasSuffix(k, "Id") && k != "requestId" {
+										k = k[:len(k)-2]
+										k = k + "._id"
 									}
 									pipeLine = append(pipeLine, bson.D{bson.E{Key: "$match", Value: bson.M{k: v}}})
 								}
@@ -920,55 +877,6 @@ func FindFilterLimit(ctx context.Context, col *mongo.Collection, result *[]bson.
 					}
 				}
 			}
-			//for _, lookup := range queryLookupList {
-			//	pipeLine = append(pipeLine, bson.D{bson.E{Key: "$lookup", Value: lookup}})
-			//}
-			//if filter, ok := query["filter"]; ok {
-			//	if filterMap, ok := filter.(bson.M); ok {
-			//		for k, v := range filterMap {
-			//			if k == "$lookup" {
-			//				pipeLine = append(pipeLine, bson.D{bson.E{Key: k, Value: v}})
-			//				// 特殊处理lookup数组
-			//			} else if k != "$lookups" {
-			//				//判断空对象
-			//				if emptyObject, ok := v.(primitive.M); ok {
-			//					flag := false
-			//					for range emptyObject {
-			//						flag = true
-			//					}
-			//					if !flag {
-			//						delete(filterMap, k)
-			//						continue
-			//					}
-			//				}
-			//				if k == "id" {
-			//					k = "_id"
-			//				}
-			//				// 递归转换判断value值是否为ObjectID
-			//				r, err := ConvertOID(k, v)
-			//				if err != nil {
-			//					return 0, fmt.Errorf(`%s的值数据检验错误.%s`, k, err.Error())
-			//				}
-			//				//if strings.HasSuffix(k, "id") && k != "_id" && !strings.HasSuffix(k, "_id") && !strings.HasSuffix(k, "uid") {
-			//				//	k = k[:len(k)-2]
-			//				//	k = k + "_id"
-			//				//} else
-			//				if hasGroup && k == "modelId" {
-			//					k = "model"
-			//				} else {
-			//					if strings.HasSuffix(k, "Id") && k != "requestId" {
-			//						k = k[:len(k)-2]
-			//						k = k + "._id"
-			//					}
-			//				}
-			//				pipeLine = append(pipeLine, bson.D{bson.E{Key: "$match", Value: bson.M{k: r}}})
-			//			}
-			//		}
-			//		// }
-			//	} else {
-			//		return 0, errors.New(`filter格式不正确`)
-			//	}
-			//}
 			if withCount, ok := query["withCount"]; ok {
 				if b, ok := withCount.(bool); ok {
 					if b {
@@ -1549,14 +1457,14 @@ func FindFilterLimit(ctx context.Context, col *mongo.Collection, result *[]bson.
 	//if project, ok := (*query)["project"]; ok {
 	//	pipeLine = append(pipeLine, bson.D{bson.E{Key: "$project", Value: project}})
 	//}
-	if err := FindPipeline(ctx, col, result, pipeLine, calc); err != nil {
+	if err := FindPipeline(ctx, col, result, pipeLine); err != nil {
 		return 0, err
 	}
 	return count, nil
 }
 
 // FindPipeline 根据mongo pipeline
-func FindPipeline(ctx context.Context, col *mongo.Collection, result *[]bson.M, pipeLine mongo.Pipeline, calc CalcPipelineFunc) error {
+func FindPipeline(ctx context.Context, col *mongo.Collection, result *[]bson.M, pipeLine mongo.Pipeline) error {
 	//增加允许使用硬盘缓存
 	AllowDiskUse := true
 	cur, err := col.Aggregate(ctx, pipeLine,
@@ -1572,10 +1480,10 @@ func FindPipeline(ctx context.Context, col *mongo.Collection, result *[]bson.M, 
 		if err != nil {
 			return err
 		}
-		if calc != nil {
-			calc(&r)
+		err = ConvertKeyID(&r)
+		if err != nil {
+			return err
 		}
-		ConvertKeyID(&r)
 		*result = append(*result, r)
 	}
 
@@ -1583,7 +1491,7 @@ func FindPipeline(ctx context.Context, col *mongo.Collection, result *[]bson.M, 
 }
 
 // FindByID 根据id查询数据
-func FindByID(ctx context.Context, col *mongo.Collection, result *bson.M, id string, calc CalcPipelineFunc) error {
+func FindByID(ctx context.Context, col *mongo.Collection, result *bson.M, id string) error {
 	singleResult := col.FindOne(ctx, bson.M{"_id": id})
 
 	if singleResult.Err() != nil {
@@ -1593,12 +1501,7 @@ func FindByID(ctx context.Context, col *mongo.Collection, result *bson.M, id str
 		return err
 	}
 
-	if calc != nil {
-		calc(result)
-	}
-
-	ConvertKeyID(result)
-	return nil
+	return ConvertKeyID(result)
 }
 
 // FindCount 根据Pipeline查询数据量
