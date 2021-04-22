c1e2d70f4e2620cfb11a79abe2c454cb2edf84ef
zhangqiang <z397435052@gmail.com>
2021-04-22 09:58:28 +0800

    fix: 模型资产内存数据

--- init/cache/model/model.go
+++ init/cache/model/model.go
@@ -3,6 +3,7 @@ package model
 import (
 	"context"
 	"fmt"
+	"math/rand"
 	"sync"
 
 	"github.com/go-redis/redis/v8"
@@ -14,66 +15,54 @@ import (
 	"github.com/air-iot/service/util/json"
 )
 
-const CacheModelPrefix = "cachemodel"
+const CacheModelPrefix = "cacheModel"
 
-type MemModel struct {
+var MemoryModelData = struct {
 	sync.Mutex
-	config      cache.CacheParams
-	mqClient    mq.MQ
-	redisClient redis.Client
-	Cache       map[string]map[string]string
-}
-
-// NewMemModel 创建模型缓存
-func NewMemModel(mqClient mq.MQ, redisClient redis.Client, config cache.CacheParams) (*MemModel, func(), error) {
-	model := new(MemModel)
-	model.redisClient = redisClient
-	model.mqClient = mqClient
-	model.config = config
-	model.config.Exchange = CacheModelPrefix
-	model.Cache = make(map[string]map[string]string)
-	cleanFunc, err := model.cacheHandler(context.Background())
-	return model, cleanFunc, err
+	Cache map[string]map[string]string
+}{
+	Cache: map[string]map[string]string{},
 }
 
 // CacheHandler 初始化model缓存
-func (p *MemModel) cacheHandler(ctx context.Context) (func(), error) {
+func CacheHandler(ctx context.Context, redisClient *redis.Client, mqCli mq.MQ, queue string) (func(), error) {
 	topic := []string{CacheModelPrefix, "#"}
 
-	ctx = context.WithValue(ctx, "exchange", p.config.Exchange)
-	ctx = context.WithValue(ctx, "queue", p.config.Queue)
+	ctx = context.WithValue(ctx, "exchange", CacheModelPrefix)
+	ctx = context.WithValue(ctx, "queue", fmt.Sprintf("model%d", rand.Int()))
 
-	if err := p.mqClient.Consume(ctx, topic, 4, func(topic1 string, topics []string, payload []byte) {
+	// 0: 前缀 1: 处理方式 2: 项目id 3: 模型id
+	if err := mqCli.Consume(ctx, topic, 4, func(topic1 string, topics []string, payload []byte) {
 		if len(topics) != 4 || topics[0] != CacheModelPrefix {
-			logger.Errorln("缓存model数据,消息主题(topic)格式错误")
+			logger.Errorln("缓存模型数据,消息主题(topic)格式错误")
 			return
 		}
 		project, id := topics[2], topics[3]
 		switch cache.CacheMethod(topics[1]) {
 		case cache.Update:
-			result, err := p.redisClient.HGet(ctx, fmt.Sprintf("%s/model", project), id).Result()
+			result, err := redisClient.HGet(ctx, fmt.Sprintf("%s/model", project), id).Result()
 			if err != nil {
-				logger.Errorf("缓存model数据,查询redis缓存[ %s %s ]错误, %s", project, id, err.Error())
+				logger.Errorf("缓存模型数据,查询redis缓存[ %s %s ]错误, %s", project, id, err.Error())
 				return
 			}
-			p.Lock()
-			modelMap, ok := p.Cache[project]
+			MemoryModelData.Lock()
+			modelMap, ok := MemoryModelData.Cache[project]
 			if ok {
 				modelMap[id] = result
 			} else {
 				modelMap = make(map[string]string)
 				modelMap[id] = result
 			}
-			p.Cache[project] = modelMap
-			p.Unlock()
+			MemoryModelData.Cache[project] = modelMap
+			MemoryModelData.Unlock()
 		case cache.Delete:
-			p.Lock()
-			modelMap, ok := p.Cache[project]
+			MemoryModelData.Lock()
+			modelMap, ok := MemoryModelData.Cache[project]
 			if ok {
 				delete(modelMap, id)
-				p.Cache[project] = modelMap
+				MemoryModelData.Cache[project] = modelMap
 			}
-			p.Unlock()
+			MemoryModelData.Unlock()
 		default:
 			return
 		}
@@ -83,7 +72,7 @@ func (p *MemModel) cacheHandler(ctx context.Context) (func(), error) {
 	}
 
 	cleanFunc := func() {
-		if err := p.mqClient.UnSubscription(ctx, topic); err != nil {
+		if err := mqCli.UnSubscription(ctx, topic); err != nil {
 			logger.Errorln("取消订阅model缓存错误,", err.Error())
 		}
 	}
@@ -92,29 +81,29 @@ func (p *MemModel) cacheHandler(ctx context.Context) (func(), error) {
 }
 
 // Get 根据项目ID和资产ID查询数据
-func (p *MemModel) Get(ctx context.Context, project, id string, result interface{}) (err error) {
-	p.Lock()
-	defer p.Unlock()
+func Get(ctx context.Context, redisClient *redis.Client, project, id string, result interface{}) (err error) {
+	MemoryModelData.Lock()
+	defer MemoryModelData.Unlock()
 	var model string
-	modelMap, ok := p.Cache[project]
+	modelMap, ok := MemoryModelData.Cache[project]
 	if ok {
 		model, ok = modelMap[id]
 		if !ok {
-			model, err = p.redisClient.HGet(ctx, fmt.Sprintf("%s/model", project), id).Result()
+			model, err = redisClient.HGet(ctx, fmt.Sprintf("%s/model", project), id).Result()
 			if err != nil {
 				return errors.WithStack(err)
 			}
 			modelMap[id] = model
-			p.Cache[project] = modelMap
+			MemoryModelData.Cache[project] = modelMap
 		}
 	} else {
-		model, err = p.redisClient.HGet(ctx, fmt.Sprintf("%s/model", project), id).Result()
+		model, err = redisClient.HGet(ctx, fmt.Sprintf("%s/model", project), id).Result()
 		if err != nil {
 			return errors.WithStack(err)
 		}
 		modelMap = make(map[string]string)
 		modelMap[id] = model
-		p.Cache[project] = modelMap
+		MemoryModelData.Cache[project] = modelMap
 	}
 
 	if err := json.Unmarshal([]byte(model), result); err != nil {
@@ -124,15 +113,14 @@ func (p *MemModel) Get(ctx context.Context, project, id string, result interface
 }
 
 // TriggerUpdate 更新redis资产数据,并发送消息通知
-func (p *MemModel) TriggerUpdate(ctx context.Context, project, id string, model []byte) error {
-	_, err := p.redisClient.HMSet(ctx, fmt.Sprintf("%s/model", project), map[string]interface{}{id: model}).Result()
+func TriggerUpdate(ctx context.Context, mqCli mq.MQ, redisClient *redis.Client, project, id string, model []byte) error {
+	_, err := redisClient.HMSet(ctx, fmt.Sprintf("%s/model", project), map[string]interface{}{id: model}).Result()
 	if err != nil {
 		return fmt.Errorf("更新redis数据错误, %s", err.Error())
 	}
 	topics := []string{CacheModelPrefix, string(cache.Update), project, id}
-	ctx = context.WithValue(ctx, "exchange", p.config.Exchange)
-	ctx = context.WithValue(ctx, "queue", p.config.Queue)
-	err = p.mqClient.Publish(ctx, topics, []byte{})
+	ctx = context.WithValue(ctx, "exchange", CacheModelPrefix)
+	err = mqCli.Publish(ctx, topics, []byte{})
 	if err != nil {
 		return fmt.Errorf("发送消息通知错误, %s", err.Error())
 	}
@@ -140,15 +128,14 @@ func (p *MemModel) TriggerUpdate(ctx context.Context, project, id string, model 
 }
 
 // TriggerDelete 删除redis资产数据,并发送消息通知
-func (p *MemModel) TriggerDelete(ctx context.Context, project, id string) error {
-	_, err := p.redisClient.HDel(context.Background(), fmt.Sprintf("%s/model", project), id).Result()
+func TriggerDelete(ctx context.Context, mqCli mq.MQ, redisClient *redis.Client, project, id string) error {
+	_, err := redisClient.HDel(context.Background(), fmt.Sprintf("%s/model", project), id).Result()
 	if err != nil {
 		return fmt.Errorf("删除redis数据错误, %s", err.Error())
 	}
 	topics := []string{CacheModelPrefix, string(cache.Delete), project, id}
-	ctx = context.WithValue(ctx, "exchange", p.config.Exchange)
-	ctx = context.WithValue(ctx, "queue", p.config.Queue)
-	err = p.mqClient.Publish(ctx, topics, []byte{})
+	ctx = context.WithValue(ctx, "exchange", CacheModelPrefix)
+	err = mqCli.Publish(ctx, topics, []byte{})
 	if err != nil {
 		return fmt.Errorf("发送消息通知错误, %s", err.Error())
 	}
--- init/cache/node/node.go
+++ init/cache/node/node.go
@@ -3,6 +3,7 @@ package node
 import (
 	"context"
 	"fmt"
+	"math/rand"
 	"sync"
 
 	"github.com/go-redis/redis/v8"
@@ -14,66 +15,54 @@ import (
 	"github.com/air-iot/service/util/json"
 )
 
-const CacheNodePrefix = "cachenode"
+const CacheNodePrefix = "cacheNode"
 
-type MemNode struct {
+var MemoryNodeData = struct {
 	sync.Mutex
-	config      cache.CacheParams
-	mqClient    mq.MQ
-	redisClient redis.Client
-	Cache       map[string]map[string]string
+	Cache map[string]map[string]string
+}{
+	Cache: map[string]map[string]string{},
 }
 
-// NewMemNode 创建Node缓存
-func NewMemNode(mqClient mq.MQ, redisClient redis.Client, config cache.CacheParams) (*MemNode, func(), error) {
-	node := new(MemNode)
-	node.redisClient = redisClient
-	node.mqClient = mqClient
-	node.config = config
-	node.config.Exchange = CacheNodePrefix
-	node.Cache = make(map[string]map[string]string)
-	cleanFunc, err := node.cacheHandler(context.Background())
-	return node, cleanFunc, err
-}
-
-// cacheHandler 初始化node缓存
-func (p *MemNode) cacheHandler(ctx context.Context) (func(), error) {
+// CacheHandler 初始化node缓存
+func CacheHandler(ctx context.Context, redisClient *redis.Client, mqCli mq.MQ, queue string) (func(), error) {
 	topic := []string{CacheNodePrefix, "#"}
 
-	ctx = context.WithValue(ctx, "exchange", p.config.Exchange)
-	ctx = context.WithValue(ctx, "queue", p.config.Queue)
+	ctx = context.WithValue(ctx, "exchange", CacheNodePrefix)
+	ctx = context.WithValue(ctx, "queue", fmt.Sprintf("node%d", rand.Int()))
 
-	if err := p.mqClient.Consume(ctx, topic, 4, func(topic1 string, topics []string, payload []byte) {
+	// 0: 前缀 1: 处理方式 2: 项目id 3: 资产id
+	if err := mqCli.Consume(ctx, topic, 4, func(topic1 string, topics []string, payload []byte) {
 		if len(topics) != 4 || topics[0] != CacheNodePrefix {
-			logger.Errorln("缓存node数据,消息主题(topic)格式错误")
+			logger.Errorln("缓存资产数据,消息主题(topic)格式错误")
 			return
 		}
 		project, id := topics[2], topics[3]
 		switch cache.CacheMethod(topics[1]) {
 		case cache.Update:
-			result, err := p.redisClient.HGet(ctx, fmt.Sprintf("%s/node", project), id).Result()
+			result, err := redisClient.HGet(ctx, fmt.Sprintf("%s/node", project), id).Result()
 			if err != nil {
-				logger.Errorf("缓存node数据,查询redis缓存[ %s %s ]错误, %s", project, id, err.Error())
+				logger.Errorf("缓存资产数据,查询redis缓存[ %s %s ]错误, %s", project, id, err.Error())
 				return
 			}
-			p.Lock()
-			nodeMap, ok := p.Cache[project]
+			MemoryNodeData.Lock()
+			nodeMap, ok := MemoryNodeData.Cache[project]
 			if ok {
 				nodeMap[id] = result
 			} else {
 				nodeMap = make(map[string]string)
 				nodeMap[id] = result
 			}
-			p.Cache[project] = nodeMap
-			p.Unlock()
+			MemoryNodeData.Cache[project] = nodeMap
+			MemoryNodeData.Unlock()
 		case cache.Delete:
-			p.Lock()
-			nodeMap, ok := p.Cache[project]
+			MemoryNodeData.Lock()
+			nodeMap, ok := MemoryNodeData.Cache[project]
 			if ok {
 				delete(nodeMap, id)
-				p.Cache[project] = nodeMap
+				MemoryNodeData.Cache[project] = nodeMap
 			}
-			p.Unlock()
+			MemoryNodeData.Unlock()
 		default:
 			return
 		}
@@ -83,7 +72,7 @@ func (p *MemNode) cacheHandler(ctx context.Context) (func(), error) {
 	}
 
 	cleanFunc := func() {
-		if err := p.mqClient.UnSubscription(ctx, topic); err != nil {
+		if err := mqCli.UnSubscription(ctx, topic); err != nil {
 			logger.Errorln("取消订阅node缓存错误,", err.Error())
 		}
 	}
@@ -92,29 +81,29 @@ func (p *MemNode) cacheHandler(ctx context.Context) (func(), error) {
 }
 
 // Get 根据项目ID和资产ID查询数据
-func (p *MemNode) Get(ctx context.Context, project, id string, result interface{}) (err error) {
-	p.Lock()
-	defer p.Unlock()
+func Get(ctx context.Context, redisClient *redis.Client, project, id string, result interface{}) (err error) {
+	MemoryNodeData.Lock()
+	defer MemoryNodeData.Unlock()
 	var node string
-	nodeMap, ok := p.Cache[project]
+	nodeMap, ok := MemoryNodeData.Cache[project]
 	if ok {
 		node, ok = nodeMap[id]
 		if !ok {
-			node, err = p.redisClient.HGet(ctx, fmt.Sprintf("%s/node", project), id).Result()
+			node, err = redisClient.HGet(ctx, fmt.Sprintf("%s/node", project), id).Result()
 			if err != nil {
 				return errors.WithStack(err)
 			}
 			nodeMap[id] = node
-			p.Cache[project] = nodeMap
+			MemoryNodeData.Cache[project] = nodeMap
 		}
 	} else {
-		node, err = p.redisClient.HGet(ctx, fmt.Sprintf("%s/node", project), id).Result()
+		node, err = redisClient.HGet(ctx, fmt.Sprintf("%s/node", project), id).Result()
 		if err != nil {
 			return errors.WithStack(err)
 		}
 		nodeMap = make(map[string]string)
 		nodeMap[id] = node
-		p.Cache[project] = nodeMap
+		MemoryNodeData.Cache[project] = nodeMap
 	}
 
 	if err := json.Unmarshal([]byte(node), result); err != nil {
@@ -124,15 +113,14 @@ func (p *MemNode) Get(ctx context.Context, project, id string, result interface{
 }
 
 // TriggerUpdate 更新redis资产数据,并发送消息通知
-func (p *MemNode) TriggerUpdate(ctx context.Context, project, id string, node []byte) error {
-	_, err := p.redisClient.HMSet(ctx, fmt.Sprintf("%s/node", project), map[string]interface{}{id: node}).Result()
+func TriggerUpdate(ctx context.Context, mqCli mq.MQ, redisClient *redis.Client, project, id string, node []byte) error {
+	_, err := redisClient.HMSet(ctx, fmt.Sprintf("%s/node", project), map[string]interface{}{id: node}).Result()
 	if err != nil {
 		return fmt.Errorf("更新redis数据错误, %s", err.Error())
 	}
 	topics := []string{CacheNodePrefix, string(cache.Update), project, id}
-	ctx = context.WithValue(ctx, "exchange", p.config.Exchange)
-	ctx = context.WithValue(ctx, "queue", p.config.Queue)
-	err = p.mqClient.Publish(ctx, topics, []byte{})
+	ctx = context.WithValue(ctx, "exchange", CacheNodePrefix)
+	err = mqCli.Publish(ctx, topics, []byte{})
 	if err != nil {
 		return fmt.Errorf("发送消息通知错误, %s", err.Error())
 	}
@@ -140,15 +128,14 @@ func (p *MemNode) TriggerUpdate(ctx context.Context, project, id string, node []
 }
 
 // TriggerDelete 删除redis资产数据,并发送消息通知
-func (p *MemNode) TriggerDelete(ctx context.Context, project, id string) error {
-	_, err := p.redisClient.HDel(context.Background(), fmt.Sprintf("%s/node", project), id).Result()
+func TriggerDelete(ctx context.Context, mqCli mq.MQ, redisClient *redis.Client, project, id string) error {
+	_, err := redisClient.HDel(context.Background(), fmt.Sprintf("%s/node", project), id).Result()
 	if err != nil {
 		return fmt.Errorf("删除redis数据错误, %s", err.Error())
 	}
 	topics := []string{CacheNodePrefix, string(cache.Delete), project, id}
-	ctx = context.WithValue(ctx, "exchange", p.config.Exchange)
-	ctx = context.WithValue(ctx, "queue", p.config.Queue)
-	err = p.mqClient.Publish(ctx, topics, []byte{})
+	ctx = context.WithValue(ctx, "exchange", CacheNodePrefix)
+	err = mqCli.Publish(ctx, topics, []byte{})
 	if err != nil {
 		return fmt.Errorf("发送消息通知错误, %s", err.Error())
 	}
