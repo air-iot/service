0b3922b3f2e4694e5e4b8503ee3c6d335259e8c1
Restia <1625016239@qq.com>
2021-05-06 17:21:05 +0800

    fix: 增加通用结构体

---
+++ init/cache/entity/global.go
@@ -0,0 +1,56 @@
+package entity
+
+import "go.mongodb.org/mongo-driver/mongo"
+
+type (
+	// GlobalModel 通用
+	GlobalModel map[string]interface{}
+	// InsertOneResult 插入结果
+	InsertOneResult mongo.InsertOneResult
+	// DeleteResult 删除结果
+	DeleteResult mongo.DeleteResult
+	// UpdateResult 更新结果
+	UpdateResult mongo.UpdateResult
+
+	DataMessage struct {
+		ModelID  string                 `json:"modelId"`
+		NodeID   string                 `json:"nodeId"`
+		Fields   map[string]interface{} `json:"fields"`
+		Uid      string                 `json:"uid"`
+		Time     int64                  `json:"time"`
+		InputMap map[string]interface{} `json:"inputMap"`
+	}
+
+	DataMessageWebsocket struct {
+		ModelID string                 `json:"modelId"`
+		NodeID  string                 `json:"nodeId"`
+		Fields  map[string]interface{} `json:"fields"`
+		Uid     string                 `json:"uid"`
+		Time    interface{}            `json:"time"`
+	}
+
+	DataMessageWithInteger struct {
+		ModelID  string                 `json:"modelId"`
+		NodeID   string                 `json:"nodeId"`
+		Fields   map[string]int         `json:"fields"`
+		Uid      string                 `json:"uid"`
+		InputMap map[string]interface{} `json:"inputMap"`
+	}
+
+	DataMessageWithInterface struct {
+		ModelID string      `json:"modelId"`
+		NodeID  string      `json:"nodeId"`
+		Fields  interface{} `json:"fields"`
+		Uid     string      `json:"uid"`
+		MsgType string      `json:"msgType"`
+	}
+
+	DataMessageWithWarnInterface struct {
+		ModelID string      `json:"modelId"`
+		NodeID  string      `json:"nodeId"`
+		Fields  interface{} `json:"fields"`
+		Uid     string      `json:"uid"`
+		MsgType string      `json:"msgType"`
+		WarnID  string      `json:"warnId"`
+	}
+)
