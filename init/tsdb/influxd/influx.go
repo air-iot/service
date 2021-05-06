985068e2156bc69ec5757a7f975c63e0f863567b
zhangqiang <z397435052@gmail.com>
2021-05-06 10:24:27 +0800

    fix: taos 配置

--- init/tsdb/influxd/influx.go
+++ init/tsdb/influxd/influx.go
@@ -1,52 +1,22 @@
 package influxd
 
 import (
-	"strings"
 	"time"
 
-	"github.com/air-iot/service/config"
-	"github.com/air-iot/service/logger"
 	client "github.com/influxdata/influxdb1-client/v2"
+
+	"github.com/air-iot/service/init/tsdb"
 )
 
 // Influx influx存储
 type Influx struct {
-	Cli client.Client
+	cli client.Client
 }
 
-// NewInflux 创建influx存储
-func NewInflux(cfg config.Influx) (client.Client, func(), error) {
-	var cli client.Client
-	var err error
-	switch strings.ToUpper(cfg.Protocol) {
-	case "UDP":
-		cli, err = client.NewUDPClient(client.UDPConfig{
-			Addr: cfg.Addr,
-		})
-	default:
-		cli, err = client.NewHTTPClient(client.HTTPConfig{
-			Addr:     cfg.Addr,
-			Username: cfg.Username,
-			Password: cfg.Password,
-		})
-	}
-
-	if err != nil {
-		return nil, nil, err
-	}
-	cleanFunc := func() {
-		err := cli.Close()
-		if err != nil {
-			logger.Errorf("influxdb close error: %s", err.Error())
-		}
-	}
-	_, _, err = cli.Ping(time.Second * 10)
-
-	if err != nil {
-		return nil, cleanFunc, err
-	}
-
-	return cli, cleanFunc, nil
+func NewInflux(cli client.Client) tsdb.TSDB {
+	a := new(Influx)
+	a.cli = cli
+	return a
 }
 
 func (a *Influx) Write(database, tableName string, ts time.Time, tags map[string]string, fields map[string]interface{}) (err error) {
--- init/tsdb/taosx/taos.go
+++ init/tsdb/taosx/taos.go
@@ -1,5 +1,41 @@
 package taosx
 
+import (
+	"database/sql"
+	"time"
+
+	"github.com/air-iot/service/config"
+	"github.com/air-iot/service/init/tsdb"
+	"github.com/air-iot/service/logger"
+)
+
 // Taos taos客户端
 type Taos struct {
+	addr string
+}
+
+// NewTaos 创建Taos存储
+func NewTaos(cfg config.Taos) tsdb.TSDB {
+	a := new(Taos)
+	a.addr = cfg.Addr
+	return a
+}
+
+func (a *Taos) Write(database, tableName string, ts time.Time, tags map[string]string, fields map[string]interface{}) (err error) {
+	db, err := sql.Open("taosSql", a.addr)
+	if err != nil {
+		return err
+	}
+	defer func() {
+		if err := db.Close(); err != nil {
+			logger.Errorf("关闭连接错误: %s", err.Error())
+		}
+	}()
+	//db.Exec(`insert into %s.n_%s(%s) USING %s.m_%s VALUES(%s)`)
+	return nil
+}
+
+func (a *Taos) Query(database string, sql string, result interface{}) (err error) {
+
+	return nil
 }
