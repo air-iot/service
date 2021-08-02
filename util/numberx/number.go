11bec849610e231d48fb4c5c0cf870ae27f2e909
zhangqiang <z397435052@gmail.com>
2021-08-02 14:22:45 +0800

    feat: 四舍五入

--- util/numberx/number.go
+++ util/numberx/number.go
@@ -2,6 +2,7 @@ package numberx
 
 import (
 	"fmt"
+	"math"
 	"math/rand"
 	"regexp"
 	"strconv"
@@ -133,4 +134,11 @@ func IsNumeric(val interface{}) bool {
 	default:
 		return false
 	}
+}
+
+// Round 四舍五入，ROUND_HALF_UP 模式实现
+// 返回将 val 根据指定精度 precision（十进制小数点后数字的数目）进行四舍五入的结果。precision 也可以是负数或零。
+func Round(val float64, precision int) float64 {
+	p := math.Pow10(precision)
+	return math.Floor(val*p+0.5) / p
 }
