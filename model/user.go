df7ef845c1a44b639269002e7f2ae5ce6f94f24a
Restia <1625016239@qq.com>
2021-11-01 11:02:56 +0800

    fix: 增加报警查询时根据用户配置筛选返回字段

--- model/user.go
+++ model/user.go
@@ -4,14 +4,15 @@ import "go.mongodb.org/mongo-driver/bson/primitive"
 
 // User
 type User struct {
-	ID             string   `json:"id"`
-	Name           string   `json:"name"`
-	Department     []string `json:"department"`
-	Roles          []string `json:"roles"`
-	OpenID         string   `json:"openid"`
-	Phone          string   `json:"phone"`
-	TimeoutTimeUse string   `json:"timeoutTimeUse"`
-	AdminAccess    bool     `json:"adminAccess"`
+	ID             string                 `json:"id"`
+	Name           string                 `json:"name"`
+	Department     []string               `json:"department"`
+	Roles          []string               `json:"roles"`
+	OpenID         string                 `json:"openid"`
+	Phone          string                 `json:"phone"`
+	TimeoutTimeUse string                 `json:"timeoutTimeUse"`
+	AdminAccess    bool                   `json:"adminAccess"`
+	PageSetting    map[string]interface{} `json:"pageSetting"`
 }
 
 // UserMongo
@@ -24,4 +25,5 @@ type UserMongo struct {
 	Phone          string               `json:"phone" bson:"phone"`
 	TimeoutTimeUse interface{}          `json:"timeoutTimeUse" bson:"timeoutTimeUse"`
 	AdminAccess    bool                 `json:"adminAccess" bson:"adminAccess"`
+	PageSetting    primitive.M          `json:"pageSetting" bson:"pageSetting"`
 }
