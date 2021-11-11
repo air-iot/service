c3c725940e756ef85ea1bddfbae73fc64ad66168
Restia <1625016239@qq.com>
2021-11-11 09:42:20 +0800

    fix: 修改工作表触发输出格式

--- flow/flow_ext.go
+++ flow/flow_ext.go
@@ -12307,12 +12307,12 @@ func TriggerExtModifyFlow(ctx context.Context, redisClient redisdb.Client, mongo
 		}
 		//=================
 		if isValid {
-			sendMap := map[string]interface{}{
-				"table": settings.Table,
-				"data":  data,
-			}
+			//sendMap := map[string]interface{}{
+			//	"table": settings.Table,
+			//	"data":  data,
+			//}
 
-			err = flowx.StartFlow(mongoClient, zbClient, flowInfo.FlowXml, projectName, string(ExtTrigger), flowID, sendMap, settings)
+			err = flowx.StartFlow(mongoClient, zbClient, flowInfo.FlowXml, projectName, string(ExtTrigger), flowID, data, settings)
 			if err != nil {
 				logger.Errorf("流程推进到下一阶段失败:%s", err.Error())
 				continue
