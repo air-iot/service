ff75e5c0094acc2d7c9be448ce5c549802261158
Restia <1625016239@qq.com>
2021-11-17 18:29:20 +0800

    fix: 增加登录用户的IP缓存

---
+++ util/login/login.go
@@ -0,0 +1,35 @@
+package login
+
+import (
+	"context"
+	"fmt"
+	"github.com/air-iot/service/init/cache"
+	"github.com/air-iot/service/init/redisdb"
+	"github.com/air-iot/service/util/json"
+	"github.com/go-redis/redis/v8"
+)
+
+// Get 根据项目ID和资产ID查询数据
+func Get(ctx context.Context, redisClient redisdb.Client, project, id string, result interface{}) (err error) {
+	nodeBytes, err := GetByDB(ctx, redisClient, project,  id)
+	if err != nil {
+		return err
+	}
+	if err := json.Unmarshal(nodeBytes, result); err != nil {
+		return err
+	}
+	return nil
+}
+
+func GetByDB(ctx context.Context, cli redisdb.Client, project, id string) ([]byte, error) {
+	model, err := cli.HGet(ctx, fmt.Sprintf("%s/%s", project, "loginIPCache"), id).Result()
+	if err != nil && err != redis.Nil {
+		return nil, err
+	}
+	return []byte(model), nil
+}
+
+// TriggerUpdate 更新redis资产数据,并发送消息通知
+func TriggerUpdate(ctx context.Context, redisClient redisdb.Client, project,id string, data interface{}) error {
+	return cache.Update(ctx, redisClient, project, "loginIPCache", id, data)
+}
