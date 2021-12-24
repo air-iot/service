638b32ffbc8c0cc48e45983b15f3d5a9ba64f433
Restia <1625016239@qq.com>
2021-12-24 11:03:21 +0800

    fix: 增加数据点map缓存获取

--- init/cache/tag/tag.go
+++ init/cache/tag/tag.go
@@ -3,6 +3,7 @@ package tag
 import (
 	"context"
 	"fmt"
+	"github.com/air-iot/service/util/json"
 
 	"go.mongodb.org/mongo-driver/mongo"
 
@@ -56,6 +57,88 @@ func FindLocalCache(ctx context.Context, redisClient redisdb.Client, mongoClient
 
 	return nil, fmt.Errorf("模型:%s 节点:%s 中未找到tag:%s", modelID, nodeID, tagID)
 }
+
+// FindLocalCache 根据模型id、节点id与数据点id查询数据点信息
+func FindLocalCacheMap(ctx context.Context, redisClient redisdb.Client, mongoClient *mongo.Client, project, modelID, nodeID, tagID string) (*map[string]interface{}, error) {
+	var modelAuto = false
+	// 查询模型tag
+	modelInfo := entity.Model{}
+	if err := model.Get(ctx, redisClient, mongoClient, project, modelID, &modelInfo); err == nil {
+		if t, err := tagModel(&modelInfo, tagID); err == nil {
+			//p.tagCache.Store(cacheID, t)
+			b,err := json.Marshal(t)
+			if err != nil{
+				return nil, err
+			}
+			tagMap := map[string]interface{}{}
+			err = json.Unmarshal(b,&tagMap)
+			if err != nil{
+				return nil, err
+			}
+			return &tagMap, err
+		}
+		modelAuto = modelInfo.Computed.Auto
+	}
+
+	nodeInfo := entity.Node{}
+	if err := node.Get(ctx, redisClient, mongoClient, project, nodeID, &nodeInfo); err == nil {
+		if t, err := tagNode(&nodeInfo, tagID); err == nil {
+			//p.tagCache.Store(cacheID, t)
+			b,err := json.Marshal(t)
+			if err != nil{
+				return nil, err
+			}
+			tagMap := map[string]interface{}{}
+			err = json.Unmarshal(b,&tagMap)
+			if err != nil{
+				return nil, err
+			}
+			return &tagMap, err
+		}
+
+		if modelAuto {
+			nodeInfoList := make([]entity.Node, 0)
+			err = node.GetByParent(ctx, redisClient, mongoClient, project, nodeID, &nodeInfoList)
+			// 查询节点子节点
+			for _, r := range nodeInfoList {
+				// 查询子节点
+				if t, err := tagNode(&r, tagID); err == nil {
+					//p.tagCache.Store(cacheID, t)
+					b,err := json.Marshal(t)
+					if err != nil{
+						return nil, err
+					}
+					tagMap := map[string]interface{}{}
+					err = json.Unmarshal(b,&tagMap)
+					if err != nil{
+						return nil, err
+					}
+					return &tagMap, err
+				}
+				// 查询子节点模型
+				m := entity.Model{}
+				if err := model.Get(ctx, redisClient, mongoClient, project, r.Model, &m); err == nil {
+					if t, err := tagModel(&m, tagID); err == nil {
+						//tagCache.Store(cacheID, t)
+						b,err := json.Marshal(t)
+						if err != nil{
+							return nil, err
+						}
+						tagMap := map[string]interface{}{}
+						err = json.Unmarshal(b,&tagMap)
+						if err != nil{
+							return nil, err
+						}
+						return &tagMap, err
+					}
+				}
+			}
+		}
+	}
+
+	return nil, fmt.Errorf("模型:%s 节点:%s 中未找到tag:%s", modelID, nodeID, tagID)
+}
+
 
 func tagModel(result *entity.Model, tagID string) (*entity.Tag, error) {
 	for _, t := range result.Device.Tags {
